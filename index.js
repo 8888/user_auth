7ffb6658f8a78a37b0a211a391c07883143b904a
betterin30days <lee@rubiconmd.com>
2019-03-31 18:35:24 -0400

    add registration functionality to make calls to endpoint

--- index.js
+++ index.js
@@ -42,3 +42,26 @@ function onClickLogin() {
     .then(login)
     .catch(logError);
 }
+
+function register(response) {
+  clearForm();
+  if (response.status !== 201) {
+    return updateMessage(response.error);
+  }
+  updateMessage('User successfully registered, please log in!');
+}
+
+function onClickRegister() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const body = {username, password};
+  const init = {
+    method: 'post',
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify(body)
+  };
+  fetch('http://localhost:8080/user/register', init)
+    .then(readResponseAsJson)
+    .then(register)
+    .catch(logError);
+}
