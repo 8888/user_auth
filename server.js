5c6236e5583c2edb46a696e7604b6f9eb8262803
betterin30days <lee@rubiconmd.com>
2019-03-31 17:42:54 -0400

    allow cors for express
    also return username when logging in as I will probably want to use this

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Auth = require('./auth.js');
 
+const allowCrossOrigin = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
 const app = express();
 app.use(bodyParser.json());
+app.use(allowCrossOrigin);
 
 const server = app.listen(process.env.PORT || 8080, () => {
   const port = server.address().port;
@@ -33,7 +41,7 @@ app.post('/user/login', async (req, res) => {
   if (credentials.username && credentials.password) {
     const result = await auth.loginUser(credentials.username, credentials.password);
     if (result.success) {
-      res.status(200).json({status: 200, token: result.token});
+      res.status(200).json({status: 200, token: result.token, username: credentials.username});
     } else {
       res.status(401).json({status: 401, error: result.message});
     }
