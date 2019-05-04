01d9caff9bb0768f95e95b8b607b1ae4f7cafd1e
betterin30days <lee@rubiconmd.com>
2019-05-03 22:34:50 -0400

    remove code from db interface that has been pulled out
    the rest of the methods in this class are being removed as well. This is more of a todo list at this point

--- app/dbInterface.js
+++ app/dbInterface.js
@@ -9,52 +9,6 @@ class DbInterface {
     });
   }
 
-  async rowExists(text, values) {
-    /**
-     * Queries the database with the provided query and values
-     * returns boolean if the row exists
-     * @text string of a SQL query
-     * @values array of values used in the query
-     */
-    try {
-      const result = await this.pool.query(text, values);
-      return !!result.rowCount;
-    } catch(err) {
-      console.log('Error executing query', err.stack);
-      return false;
-    }
-  }
-
-  async userNameExists(username) {
-    const text = `
-      SELECT username
-      FROM user_auth.auth
-      WHERE username=$1;
-    `;
-    const values = [username];
-    return await this.rowExists(text, values);
-  }
-
-  async userAndPassMatches(username, passHash) {
-    const text = `
-      SELECT id
-      FROM user_auth.auth
-      WHERE username=$1 AND pass_hash=$2;
-    `;
-    const values = [username, passHash];
-    return await this.rowExists(text, values);
-  }
-
-  async userIsAuthorized(username, token) {
-    const text = `
-      SELECT id
-      FROM user_auth.auth
-      WHERE username=$1 AND token=$2;
-    `;
-    const values = [username, token];
-    return await this.rowExists(text, values);
-  }
-
   async setToken(username, token) {
     const text = `
       UPDATE user_auth.auth
