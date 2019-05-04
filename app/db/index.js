1a0d84c90a17f47c20f65edbd741a07205c8b57a
betterin30days <lee@rubiconmd.com>
2019-05-03 21:34:20 -0400

    create exists method on db

--- app/db/index.js
+++ app/db/index.js
@@ -3,5 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({ connectionString: 'postgresql://localhost/user_auth' });
 
 module.exports = {
-  query: (text, params, callback) => pool.query(text, params, callback)
+  query: (text, params, callback) => pool.query(text, params, callback),
+  exists: async (sqlObject) => {
+    sqlObject.text = `SELECT EXISTS(${sqlObject.text})`;
+    const result = await pool.query(sqlObject);
+    return result.rows[0].exists;
+  }
 }
