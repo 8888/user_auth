0348849dfd1e8ff39db18f5f8838cc559677f3b5
betterin30days <lee@rubiconmd.com>
2019-05-11 12:06:48 -0400

    remove unused dependency from app

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const usersRouter = require('./routes/users');
-const auth = require('./auth.js');
 
 const allowCrossOrigin = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
